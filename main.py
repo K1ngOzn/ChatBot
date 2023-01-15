4d3ddbbab4e4f3c96c6370425f68e383f291756d
ErikCarbajal404 <>
2023-01-15 16:56:17 +0000

    Added more while loops with def to make it look nice.

--- main.py
+++ main.py
@@ -1,34 +1,54 @@
-name = input("what's your name: ")
-print("that's a good name, " +name+"")
-question = input(""+name+", would you like to know about our store hours, locations, available products, or prices, if so plese choose a topic: ")
-if question == ('store hours'):
-  print('Our store is open from 6:00AM to 12:00PM all week')
-if question == ('locations'):
-  print('We have 10,000 stores located around the U.S and have many stores near your 10 mile radius')
-if question == ('available products'):
-  print('We have fresh produce and food items in our stores whith the most trusted brands: ')
-if question == ('prices'):
-  print('We have afordable prices for the average consumer and offer 10% discounts on the weekends')
-end = input('If you are satisfied with our answers plese type stop, if not plese type continue: ')
-stop = "stop"
-if end == ('stop'):
-  print('have a good day, '+name+'')
-while end != stop:
-  choose = input('ask another question: ')
-  while choose != stop:
-   if question == ('store hours'):
-     print('Our store is open from 6:00AM to 12:00PM all week')
-     break
-   if question == ('locations'):
-     print('We have 10,000 stores located around the U.S and have many stores near your 10 mile radius')
-     break
-   if question == ('available products'):
-     print('We have fresh produce and food items in our stores whith the most trusted brands: ')
-     break
-   if question == ('prices'):
-     print('We have afordable prices for the average consumer and offer 10% discounts on the weekends')
-     break
-  end = input('If you are satisfied with our answers plese type stop: ')
-  
-  if end == stop:
+def dash():
+  print("-------------------------------")
+
+
+print("Welcome to Erik's Shop")
+name = input("what is you name?: ")
+dash()
+print(f"Hello {name}")
+print("what can i help you in?")
+question = input("-Prices\n-Locations\n-Store Hours\n-Products\n-Exit\n\n")
+question.lower()
+while question.lower(
+) == "prices" or "locations" or "store hours" or "products" or "exit":
+  if question.lower() == "prices":
+    print(
+      "We have afordable prices for the average consumer and offer 10% discounts on the weekends"
+    )
+    dash()
+    question = input(
+      "choose something else\n-Prices\n-Locations\n-Store Hours\n-Products\n-Exit\n"
+    )
+  elif question.lower() == "locations":
+    print(
+      "We have 10,000 stores located around the U.S and have many stores near your 10 mile radius"
+    )
+    dash()
+    question = input(
+      "choose something else\n-Prices\n-Locations\n-Store Hours\n-Products\n-Exit\n"
+    )
+  elif question.lower() == "store hours":
+    print("Our store is open from 6:00AM to 12:00PM all week")
+    dash()
+    question = input(
+      "choose something else\n-Prices\n-Locations\n-Store Hours\n-Products\n-Exit\n"
+    )
+  elif question.lower() == "products":
+    print(
+      "We have fresh produce and food items in our stores whith the most trusted brands"
+    )
+    dash()
+    question = input(
+      "choose something else\n-Prices\n-Locations\n-Store Hours\n-Products\n-Exit\n"
+    )
+  elif question.lower() == "exit":
     break
+  else:
+    print(
+      "We can not answer your question right now, but you could call our store number for further questions"
+    )
+    dash()
+    question = input(
+      "choose something else\n-Prices\n-Locations\n-Store Hours\n-Available Products\n-Exit\n"
+    )
+print(f"Thank you {name} for visiting Erik's Shop\nHope to see you soon")
